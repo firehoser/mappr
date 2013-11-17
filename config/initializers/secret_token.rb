9161f48c1d1a8f39f9e1f99f1ac62623fa68eea4
Firehose <firehose@Kens-MacBook-Pro.local>
2013-11-17 14:34:01 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = 'a2a30e6f03e4cd9c7d7887d17fd19b6c6f040e89c3fc58a7abd6036e99a313266b1f05c105a223a8ac2ff305c01166d15a6517653682a4b3819bf3791007d077'
